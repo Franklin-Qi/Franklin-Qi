067d37ce4e6fa361b63c34c391859f3dbb37b427
yushuoqi <yushuoqi@kylinos.cn>
2023-03-28 11:38:22 +0800

    update main.py

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ import re
 import pytz
 from datetime import datetime
 
-def get_link_info(feed_url, num):
-
-    result = ""
-    feed = feedparser.parse(feed_url)
-    feed_entries = feed["entries"]
-    feed_entries_length = len(feed_entries)
-    all_number = 0;
-
-    if(num > feed_entries_length):
-        all_number = feed_entries_length
-    else:
-        all_number = num
-    
-    for entrie in feed_entries[0: all_number]:
-        title = entrie["title"]
-        link = entrie["link"]
-        result = result + "\n" + "[" + title + "](" + link + ")" + "\n"
-    
-    return result
-    
 def main():
 
     # replace content between `---start---` and `---end---`
